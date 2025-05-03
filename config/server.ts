0fc2b15083abc63c3a92f39bd04dc62dffc37fab
Mlook70 <Abdulmalek.akel@gmail.com>
2025-05-03 12:11:22 +0300

    fix: configure app.keys from .env for deployment

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('16b1c9bab482412754c8b4992326e92572358cde882cb868ecb6496422d30671','d4d7d6a2061ba7319c938ab84f858f0b65fee96991c23283033312de2081e345'),
+    keys: env.array('API_KEY1','APP_KEY2'),
   },
 });
